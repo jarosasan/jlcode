2391e94709d6e0ed531e9e21e6061f67f451ca00
jaroslavlomecki <jaroslavlomecki@jaroslaeckisAir.lan>
2018-07-28 15:28:03 +0300

    Las Commit

--- gulpfile.js
+++ gulpfile.js
@@ -52,12 +52,12 @@ gulp.task('rsync', function() {
             root: 'app/',
             hostname: 'username@yousite.com',
             destination: 'yousite/public_html/',
-            // include: ['*.htaccess'], // Includes files to deploy
+            include: ['*.htaccess'], // Includes files to deploy
             exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
             recursive: true,
             archive: true,
             silent: false,
-            compress: true
+            compress: false
         }))
 });
 
